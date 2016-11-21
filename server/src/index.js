91d594e3b7332cb06862433c3f6a84593cd777a1
Administrator <admin@example.com>
2016-11-21 19:16:21 +0800

    temp

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 import Koa from 'koa'
+import router from 'koa-router'
+const Router = router()
 const app = new Koa()
 
-app.use(ctx => {
+Router.get('/', ctx => {
   ctx.body = 'hello node'
 })
+app.use(Router.routes())
 
 app.listen(3000, () => {
   console.log('listening 3000')
